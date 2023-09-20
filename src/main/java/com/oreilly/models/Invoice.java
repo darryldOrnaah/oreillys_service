06ae04f7576db341cf9a1a264505a52f4245e865
Darryl Dover <42664068+darryldOrnaah@users.noreply.github.com>
2023-09-20 11:23:40 -0400

    Update Invoice.java

--- src/main/java/com/oreilly/models/Invoice.java
+++ src/main/java/com/oreilly/models/Invoice.java
@@ -17,27 +17,44 @@ public class Invoice {
     private Long invoiceId;
     private String tenderType;
 
-
+    /**
+    * get the id of the record
+    */
     public Long getId() {
         return id;
     }
 
+    /**
+    * sets the id of the record
+    */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+    * get the id of the customer
+    */
     public Long getCustomerId() {
         return customerId;
     }
 
+    /**
+    * sets the id of the customer
+    */
     public void setCustomerId(Long customerId) {
         this.customerId = customerId;
     }
 
+    /**
+    * get the id of the invoice
+    */
     public Long getInvoiceId() {
         return invoiceId;
     }
 
+    /**
+    * sets the id of the invoice
+    */
     public void setInvoiceId(Long invoiceId) {
         this.invoiceId = invoiceId;
     }
