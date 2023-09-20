81b43b6331201821c9449ce25dc38fd143e66439
Darryl Dover <42664068+darryldOrnaah@users.noreply.github.com>
2023-09-20 11:29:18 -0400

    Update InvoiceController.java

--- src/main/java/com/oreilly/controllers/InvoiceController.java
+++ src/main/java/com/oreilly/controllers/InvoiceController.java
@@ -18,6 +18,6 @@ public class InvoiceController {
 
     @GetMapping(path = "/all")
     public Map<Long, String> getInvoices(@RequestParam(value = "customerId")) {
-
+        return invoiceRepository.findAll();
     }
 }
