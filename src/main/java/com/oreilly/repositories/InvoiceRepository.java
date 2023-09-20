7622e023e0f97ce205973be760638d457ba6d292
Darryl Dover <42664068+darryldOrnaah@users.noreply.github.com>
2023-09-20 13:36:45 -0400

    Update InvoiceRepository.java

--- src/main/java/com/oreilly/repositories/InvoiceRepository.java
+++ src/main/java/com/oreilly/repositories/InvoiceRepository.java
@@ -1,6 +1,7 @@
 package com.oreilly.repositories;
 
+import com.oreilly.models.Invoice;
 import org.springframework.data.repository.CrudRepository;
 
-public interface InvoiceRepository extends CrudRepository<Long, String> {
+public interface InvoiceRepository extends CrudRepository<Invoice, String> {
 }
