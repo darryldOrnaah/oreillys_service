f17840d7a516e90d0376d80048a136d3f99bc1cf
Darryl Dover <42664068+darryldOrnaah@users.noreply.github.com>
2023-09-20 11:28:03 -0400

    Update OreillyServiceApplication.java

--- src/main/java/com/oreilly/oreillyservice/OreillyServiceApplication.java
+++ src/main/java/com/oreilly/oreillyservice/OreillyServiceApplication.java
@@ -3,6 +3,9 @@ package com.oreilly.oreillyservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+* Class for the application
+*/
 @SpringBootApplication
 public class OreillyServiceApplication {
 
